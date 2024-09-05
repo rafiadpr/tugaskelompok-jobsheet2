a7a0e4b5b8beacb205697976ee4809094dd98100
Rizkihadiono <rizkihadiono95@gmail.com>
2024-09-05 14:08:58 +0700

    add biodata rizki

--- BiodataKelompok.java
+++ BiodataKelompok.java
@@ -2,5 +2,8 @@ public class BiodataKelompok {
 
     public static void main(String[] args) {
         System.out.println("Biodata Kelompok");
+        System.out.println("Nama  : Mokhamad Rizki Hadiono Singgih");
+        System.out.println("NIM   : 244107020198");
+        System.out.println("Kelas : 1F");
     }
 }
