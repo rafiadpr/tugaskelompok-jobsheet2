6cdbded4b6b6d5e40cc7116f4cef19ab68f5f702
rafiadpr <work.rafiadrianp@gmail.com>
2024-09-05 13:25:52 +0700

    add biodata andre

--- BiodataKelompok.java
+++ BiodataKelompok.java
@@ -2,5 +2,8 @@ public class BiodataKelompok {
 
     public static void main(String[] args) {
         System.out.println("Biodata Kelompok");
+        System.out.println("Nama : Rafi Adrian Prasetya");
+        System.out.println("NIM : 244107020026");
+        System.out.println("Kelas : 1F");
     }
 }
