98cf8259a33dd3e01a6195c43bc0daffb5bad3f3
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2024-09-10 09:42:29 +0700

    Branch AWAL

---
+++ biodata.java
@@ -0,0 +1,9 @@
+
+public class biodata {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Nabhan Rizqi Julian Saputro");
+        System.out.println("Kelas 1F TI");
+        System.out.println("Mahasiswa Politeknik Negeri Malang ");
+    }
+    
+}
